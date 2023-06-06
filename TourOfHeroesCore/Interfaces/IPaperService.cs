f419ddd74ae713581977f9c97f93958b4218709c
mdy6 <racassin.mathieu@gmail.com>
2023-06-06 16:10:29 +0200

    add method on paper service

--- TourOfHeroesCore/Interfaces/IPaperService.cs
+++ TourOfHeroesCore/Interfaces/IPaperService.cs
@@ -5,8 +5,10 @@ namespace TourOfHeroesCore.Interfaces
     public interface IPaperService
     {
         Task<Paper> GetPaperById(Id<int> idInt);
+        Task<Paper[]> GetPapersByHeroId(Id<int> heroId);
         Task<IdInt> Publish(Paper paper);
         Task<IdInt> UpdatePaper(Paper existingPaper);
         Task DeletePaperByHeroId(Id<int> heroId);
+
     }
 }
